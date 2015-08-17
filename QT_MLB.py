16c225f3d50252de350e1c87a4f4350b83265774
slickness <randyh1981@gmail.com>
2015-08-16 20:18:11 -0400

    add qt widget gui

--- QT_MLB.py
+++ QT_MLB.py
@@ -1,19 +1,82 @@
 #!/usr/bin/env python
+import time
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore, Qt
+import MLB
+games = []
+game= []
+x = 0
+class Window(QtGui.QWidget):
+    def __init__(self):
+        super(Window,self).__init__()
+        self.setGeometry(50,50,200,100)
+        self.setWindowTitle("MLB Scores")
+        self.home()
+        self.thread = Worker()
 
-class HelloWindow(QtGui.QMainWindow):
+    def home(self):
+        btn =  QtGui.QPushButton("Get Score",self)
+        btn.move(65,0)
+        #btn.resize(50,20)
+        btn.clicked.connect(self.score)
+        self.LabelStatus = QtGui.QLabel("Status",self)
+        self.LabelStatus.move(10,25)
+        self.LabelVis =QtGui.QLabel("Visitor",self)
+        self.LabelVis.move(10,50)
+        self.LableVScore = QtGui.QLabel("vscore",self)
+        self.LableVScore.move(150,50)
+        self.LabelHome =QtGui.QLabel("Home",self)
+        self.LabelHome.move(10,75)
+        self.LableHScore = QtGui.QLabel("hscore",self)
+        self.LableHScore.move(150,75)
+        self.worker = Worker()
+        self.connect(self.worker,QtCore.SIGNAL("ThreadDone()"),self.ShowScores,QtCore.Qt.DirectConnection)
+        self.LabelStatus.setFixedWidth(100)
+        self.LabelHome.setFixedWidth(200)
+        self.LabelVis.setFixedWidth((200))
+        self.LableHScore.setFixedWidth(50)
+        self.LableVScore.setFixedWidth(50)
+        #btn.resize(100,100)
+        #btn.move(100,100)
 
-    def __init__(self,win_parent = None):
-        #init the base class
-        QtGui.QMainWindow.__init__(self,win_parent)
-    def create_widgets(self):
-        central_widget = QtGui.QWidget()
-        self.setCentralWidget(central_widget)
-        
+        self.show()
+    def score(self):
+        self.worker.start()
+        #self.LabelHome.setText(str("test"))
+    def ShowScores(self):
+        global x
+        global game
+        self.LabelStatus.setText(game[4])
+
+        self.LabelHome.setText(game[2])
+
+        self.LabelVis.setText(game[0])
+
+        self.LableHScore.setText(game[3])
+
+
+        self.LableVScore.setText(game[1])
+
+        Qt.QApplication.processEvents()
+class Worker(QtCore.QThread):
+
+    def __init__(self, parent = None):
+
+        QtCore.QThread.__init__(self, parent)
+
+
+    def run(self):
+        global x
+        global games
+        global game
+        url ="http://www.sportsnet.ca/baseball/mlb/scores/"
+        games = MLB.main(url)
+        for game in games:
+            time.sleep(5)
+            #print  "over with thread"
+
+            self.emit(QtCore.SIGNAL("ThreadDone()"))
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    #the main window
-    main_window = HelloWindow()
-    main_window.show()
-    app.exec_()
+    GUI = Window()
+    sys.exit(app.exec_())
